594b9824fe597ecf4788adefe0c4ea759db95b37
Zorgosto <34967740+Zorgosto@users.noreply.github.com>
2023-03-02 18:05:13 +0100

    reate react app

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        Hey, this is my ethereum web app where you can send me a wave! You check how often oyu have waived so far.
         </div>
 
         <button className="waveButton" onClick={wave}>
